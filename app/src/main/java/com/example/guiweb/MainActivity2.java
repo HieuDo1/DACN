9b0c6a6b922b9484906ad8cc188cc6f2f246c9a6
Hieu <manhhieudo0@gmail.com>
2023-11-10 14:37:14 +0700

    ThemMatKhau

--- app/src/main/java/com/example/guiweb/MainActivity2.java
+++ app/src/main/java/com/example/guiweb/MainActivity2.java
@@ -1,12 +1,14 @@
 package com.example.guiweb;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
@@ -16,12 +18,11 @@ import com.google.zxing.integration.android.IntentResult;
 
 
 public class MainActivity2 extends AppCompatActivity {
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
 
-    Button bt, bt3;
-    TextView tv, tv1, tv2, tv3, tv4;
+    private Button bt;
+    TextView tv, tvmk;
 
-
-    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,53 +30,98 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         bt = findViewById(R.id.bt);
-        bt3 = findViewById(R.id.bt3);
         tv = findViewById(R.id.tv);
-        tv1 = findViewById(R.id.tv1);
-        tv2= findViewById(R.id.tv2);
-        tv3 = findViewById(R.id.tv3);
-        tv4 = findViewById(R.id.tv4);
+        tvmk = findViewById(R.id.tvmk);
 
-
-
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
-
-        bt.setOnClickListener(new View.OnClickListener() {
+        bt .setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                // Kiểm tra quyền truy cập máy ảnh
                 IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity2.this);
                 intentIntegrator.setOrientationLocked(true);
-                intentIntegrator.setPrompt("Quet ma QR");
-                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-                intentIntegrator.initiateScan();
 
-            }
-        });
-
-        bt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i1 = new Intent(MainActivity2.this,MainActivity3.class);
-                startActivity(i1);
+                startQRScan();
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
-    {
-        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(intentResult != null){
-            String contents = intentResult.getContents();
-            if(contents != null){
-                tv.setText(intentResult.getContents());
-                mDatabase.child("Laysp").setValue(intentResult.getContents());
-            }
+    private void startQRScan() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        integrator.setPrompt("Quét mã QR");
+        integrator.setCameraId(0);  // Sử dụng camera sau
+        integrator.initiateScan();
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Lấy kết quả từ IntentIntegrator
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (result != null) {
+            if (result.getContents() != null) {
+                String qrValue = result.getContents();
+                tv.setText("Đồ của bạn đã được lưu vào kho "+qrValue);
+                // Lưu giá trị mã QR vào Firebase
+                saveQRValueToFirebase(qrValue);
+
+                // Tạo mật khẩu ngẫu nhiên
+                String randomPassword = generateRandomPassword();
+
+                tvmk.setText("Mật khẩu của bạn là: "+randomPassword);
+                // Lưu mật khẩu vào Firebase tương ứng với giá trị mã QR
+                if (qrValue.equals("kho 1")) {
+                    savePasswordToFirebase("matkhaukho1", randomPassword);
+                }
+                else if (qrValue.equals("kho 2")) {
+                    savePasswordToFirebase("matkhaukho2", randomPassword);
+                }
+                else if (qrValue.equals("kho 3")) {
+                    savePasswordToFirebase("matkhaukho3", randomPassword);
+                }
+                else if (qrValue.equals("kho 4")) {
+                    savePasswordToFirebase("matkhaukho4", randomPassword);
+                }
+                // Hiển thị thông báo thành công
+                Toast.makeText(this, "Mã QR đã được quét và lưu vào Firebase.", Toast.LENGTH_SHORT).show();
+
+            }
         }
-        else{
-            super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void saveQRValueToFirebase(String qrValue) {
+        // Thay đổi 'your-firebase-database' thành tên thực tế của Firebase Database của bạn
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("LenhGui");
+        databaseReference.setValue(qrValue);
+    }
+
+    private void savePasswordToFirebase(String key, String password) {
+        // Thay đổi 'your-firebase-database' thành tên thực tế của Firebase Database của bạn
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child(key);
+        databaseReference.setValue(password);
+    }
+
+    private String generateRandomPassword() {
+        // Tạo mật khẩu ngẫu nhiên gồm 4 số
+        StringBuilder password = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            int randomDigit = (int) (Math.random() * 10);
+            password.append(randomDigit);
+        }
+        return password.toString();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Tiếp tục quét mã QR sau khi đã được cấp quyền truy cập máy ảnh
+                startQRScan();
+            } else {
+                Toast.makeText(this, "Ứng dụng không được cấp quyền truy cập máy ảnh.", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
--- app/src/main/java/com/example/guiweb/MainActivity4.java
+++ app/src/main/java/com/example/guiweb/MainActivity4.java
@@ -1,13 +1,23 @@
 package com.example.guiweb;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -30,29 +40,244 @@ public class MainActivity4 extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                databaseRef.child("Lenh").setValue("sp1");
+                showPasswordDialog1();
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                databaseRef.child("Lenh").setValue("sp2");
+                showPasswordDialog2();
             }
         });
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                databaseRef.child("Lenh").setValue("sp3");
+
+
+                showPasswordDialog3();
             }
         });
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                databaseRef.child("Lenh").setValue("Sp4");
+                showPasswordDialog4();
             }
         });
 
         }
+        // kho1
+    private void showPasswordDialog1() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Nhập mật khẩu của bạn");
+
+        final EditText passwordEditText = new EditText(this);
+        builder.setView(passwordEditText);
+
+        builder.setPositiveButton("Xác nhận", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String password = passwordEditText.getText().toString().trim();
+                if (!TextUtils.isEmpty(password)) {
+                    checkPassword1(password);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void checkPassword1(final String password) {
+        databaseRef.child("matkhaukho1").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String firebasePassword = dataSnapshot.getValue(String.class);
+                if (firebasePassword != null && firebasePassword.equals(password))
+                {
+                    sendCommandToFirebase("kho1");
+                } else
+                {
+
+                    Toast.makeText(MainActivity4.this, "Sai mật khẩu ", Toast.LENGTH_SHORT).show();
+                    // Mật khẩu không đúng
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Xử lý lỗi
+            }
+        });
+    }
+
+    // kho2
+    private void showPasswordDialog2() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Nhập mật khẩu của bạn");
+
+        final EditText passwordEditText = new EditText(this);
+        builder.setView(passwordEditText);
+
+        builder.setPositiveButton("Xác nhận", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String password = passwordEditText.getText().toString().trim();
+                if (!TextUtils.isEmpty(password)) {
+                    checkPassword2(password);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void checkPassword2(final String password) {
+        databaseRef.child("matkhaukho2").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String firebasePassword = dataSnapshot.getValue(String.class);
+                if (firebasePassword != null && firebasePassword.equals(password)) {
+                    sendCommandToFirebase("kho2");
+                } else {
+
+                    Toast.makeText(MainActivity4.this, "Sai mật khẩu ", Toast.LENGTH_SHORT).show();
+                    // Mật khẩu không đúng
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Xử lý lỗi
+            }
+        });
+    }
+
+    // kho3
+    private void showPasswordDialog3() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Nhập mật khẩu của bạn");
+
+        final EditText passwordEditText = new EditText(this);
+        builder.setView(passwordEditText);
+
+        builder.setPositiveButton("Xác nhận", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String password = passwordEditText.getText().toString().trim();
+                if (!TextUtils.isEmpty(password)) {
+                    checkPassword3(password);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void checkPassword3(final String password) {
+        databaseRef.child("matkhaukho3").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String firebasePassword = dataSnapshot.getValue(String.class);
+                if (firebasePassword != null && firebasePassword.equals(password)) {
+                    sendCommandToFirebase("kho3");
+                } else {
+
+                    Toast.makeText(MainActivity4.this, "Sai mật khẩu ", Toast.LENGTH_SHORT).show();
+                    // Mật khẩu không đúng
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Xử lý lỗi
+            }
+        });
+    }
+
+    // kho4
+    private void showPasswordDialog4() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Nhập mật khẩu của bạn");
+
+        final EditText passwordEditText = new EditText(this);
+        builder.setView(passwordEditText);
+
+        builder.setPositiveButton("Xác nhận", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String password = passwordEditText.getText().toString().trim();
+                if (!TextUtils.isEmpty(password)) {
+                    checkPassword4(password);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void checkPassword4(final String password) {
+        databaseRef.child("matkhaukho4").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String firebasePassword = dataSnapshot.getValue(String.class);
+                if (firebasePassword != null && firebasePassword.equals(password)) {
+                    sendCommandToFirebase("kho4");
+                } else {
+
+                    Toast.makeText(MainActivity4.this, "Sai mật khẩu ", Toast.LENGTH_SHORT).show();
+                    // Mật khẩu không đúng
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Xử lý lỗi
+            }
+        });
+    }
+
+    private void sendCommandToFirebase(String command) {
+        databaseRef.child("LenhLay").setValue(command)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // Gửi thành công
+                        } else {
+                            // Gửi thất bại
+                        }
+                    }
+                });
+    }
+
 }
+
