c5c3f392c010cbc64af4193a66b7caa44df61975
Hieu <manhhieudo0@gmail.com>
2023-11-29 23:17:24 +0700

    ChinhSuaGiaoDien

--- app/src/main/java/com/example/guiweb/MainActivity.java
+++ app/src/main/java/com/example/guiweb/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.guiweb;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         bt3 = findViewById(R.id.bt3);
         bt4 = findViewById(R.id.bt4);
